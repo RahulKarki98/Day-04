c4aad2b4cadd488b33a0d71448e63a50f7bd2b89
Rahul Karki <51796932+RahulKarki98@users.noreply.github.com>
2022-06-21 17:45:51 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,21 @@
+// Use the  rest countries API and display all countries flags
+
+    var request =new XMLHttpRequest();
+    request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+    request.send();
+   request.onload=function()
+{
+    var result=JSON.parse(request.response);
+    console.log(result);
+    
+      var result = JSON.parse(request.response);
+    console.log(result);
+   var res= result.map((country)=>({
+       flags:country.flag,
+       
+    }));
+    console.log(res);
+};
+
+  
+};
